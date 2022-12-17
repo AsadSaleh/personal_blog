040277a55fe6b437659d07d28cc373a237aa6350
bramantyociptaadi <bramantyociptaadi@gmail.com>
2022-12-17 11:24:43 +0700

    menambahkan hobby

--- app/routes/__dashboard/toys-n-hobbies.tsx
+++ app/routes/__dashboard/toys-n-hobbies.tsx
@@ -2,6 +2,10 @@ export default function ToysAndHobbies() {
   return (
     <div>
       <h1 className="text-3xl">Toys And Hobbies</h1>
+      <ul>
+        <li>main bola</li>
+        <li>merakit gundam</li>
+      </ul>
     </div>
   );
 }
